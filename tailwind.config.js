6c14738a6ea55eaeb4c0bc1dfbb6c48d44350ac5
Tella Boluwatife <boluwatifetella@gmail.com>
2023-07-24 14:19:12 +0100

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+      lato: ["Lato", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
