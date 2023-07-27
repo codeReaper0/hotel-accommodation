1b7334a530f7dbaddfd3365ad701b61f1904eb87
Tella Boluwatife <boluwatifetella@gmail.com>
2023-07-27 16:23:30 +0100

    privacy and policy

--- src/app/policy/page.tsx
+++ src/app/policy/page.tsx
@@ -1,5 +1,88 @@
+import Header from "@/components/roomHeader";
 import React from "react";
 
 export default function Policy() {
-  return <div>Poli</div>;
+  return (
+    <>
+      <Header />
+      <section className="p-10 bg-gray-100">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex justify-between items-center px-20 py-5">
+            <h1 className="text-4xl uppercase font-bold my-10 text-center">
+              Privacy and Policy
+            </h1>
+
+            <p className="text-lg text-justify">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. A illo
+              minus fuga accusamus maxime, sit saepe laborum ratione repellat
+              incidunt dolore aperiam rem iusto sapiente officia dolor deleniti
+              vero exercitationem. Quas repellendus veritatis laudantium
+              reiciendis qui laborum ut voluptatem quis? Molestiae minima
+              dolorum quos minus velit veniam voluptatem, aliquam obcaecati
+              beatae id atque accusamus. Perspiciatis consectetur eum nam
+              cupiditate adipisci. Dolorem praesentium tempore ad. Rerum nisi
+              sunt itaque autem voluptate tenetur totam facilis? Eius officia
+              quia harum perspiciatis saepe aperiam voluptatem aut, maxime fugit
+              obcaecati accusantium enim fuga nostrum quae. Aspernatur pariatur
+              cumque error rem quam eius asperiores mollitia similique aliquam!
+              Quae officia eum dolorum aspernatur perferendis eaque, quidem
+              temporibus itaque alias reprehenderit doloremque magni quam
+              perspiciatis neque expedita facere? Minima quasi accusantium
+              dolorum qui praesentium in vero! Optio aliquam accusantium enim
+              fugit illum voluptatum eaque molestias, cupiditate alias officia
+              perferendis fugiat amet autem ad assumenda nulla quas. Adipisci,
+              quaerat. Saepe praesentium neque sapiente asperiores vero qui vel
+              odio! Blanditiis voluptatem autem aperiam, veniam animi impedit
+              incidunt illum, odit eum mollitia similique perferendis optio esse
+              ut aliquam ullam fuga et? Voluptates quod quaerat eum in a soluta
+              at possimus excepturi deleniti. Soluta rerum in nobis? Fugit,
+              laudantium deserunt. Corporis autem vitae quos cum pariatur
+              tempore, sint aperiam itaque vel quas. Quisquam assumenda tempora
+              ipsa natus. Sint quam nobis magnam mollitia maxime asperiores
+              fugit, dolore quis voluptate recusandae eveniet vero voluptatem,
+              incidunt voluptas quae quasi maiores impedit quaerat aliquam. Non,
+              aliquid. Ab eum necessitatibus non laudantium ea nam, aliquid
+              dicta animi voluptatum excepturi et eaque esse ad in voluptate hic
+              eius, rem rerum facere. Doloribus, sint totam ab error optio
+              accusamus! Nobis cupiditate eum, hic quam maxime corporis cumque
+              impedit autem nesciunt fugiat asperiores mollitia accusantium
+              enim. Suscipit beatae aliquid sequi, ad quam, eum unde rem
+              repellat sed nostrum aperiam pariatur. Non, tempora quidem
+              dignissimos facere dicta corporis modi aspernatur autem animi
+              distinctio laboriosam quas at recusandae asperiores earum. Sint
+              reprehenderit corporis rerum quam distinctio repellat voluptas
+              debitis blanditiis, numquam aperiam! Ea laboriosam dolorum non
+              obcaecati odit illo aperiam in nesciunt inventore beatae,
+              cupiditate facilis quasi, praesentium debitis quas dolore animi.
+              Atque iure unde modi quos! Molestiae voluptatum quidem nam amet.
+              Repellat tenetur omnis sed obcaecati hic maxime saepe pariatur!
+              Qui reiciendis, cum ullam a culpa autem id asperiores odio tenetur
+              eum, libero dolorem itaque? Ut harum doloremque aliquid cupiditate
+              architecto! Iste quidem sit molestiae iusto quos hic inventore nam
+              odio! Non officia sed provident ut quaerat cumque sapiente quasi?
+              Assumenda labore voluptas deleniti fugit libero quos perferendis,
+              enim magnam vel! Similique facilis omnis ratione hic delectus!
+              Magnam obcaecati provident, asperiores quo veniam sed officia
+              dolore facilis porro enim sunt, recusandae ea voluptates eligendi
+              ullam deleniti officiis iure illum sint quos. Ea enim quisquam,
+              commodi perferendis omnis labore laudantium odio tenetur! Mollitia
+              cum inventore eum autem, illo facilis distinctio. Ex saepe cum
+              earum placeat sed iure voluptatibus atque quo impedit quibusdam.
+              Ab nobis natus repellat repudiandae! Eius, nobis unde hic aperiam
+              dolor consequatur sed dolores a, beatae neque doloribus? Ut quas
+              amet tempore ullam dolore ratione atque debitis ab odio animi?
+              Consectetur rem aut soluta vitae cum neque voluptas itaque?
+              Veritatis quasi voluptates repudiandae, aspernatur quidem
+              molestiae sapiente eaque optio accusantium fugit repellendus
+              quisquam culpa facere nulla incidunt quibusdam nostrum temporibus!
+              Vero nesciunt doloribus ipsa aut exercitationem, sit iste
+              laudantium omnis animi mollitia eius odio, laboriosam commodi
+              corrupti sunt distinctio eum, sapiente similique error quia fugit
+              molestiae. Ab quas officiis minima.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
